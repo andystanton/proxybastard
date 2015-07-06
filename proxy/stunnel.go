26eb25e865a2dc4ac937881e5d473b2710b70112
Andy Stanton <andystanton@gmail.com>
2015-07-06 17:56:34 +0100

    Add functionality for adding stunnel proxy settings

--- proxy/stunnel.go
+++ proxy/stunnel.go
@@ -1,1 +1,38 @@
 package proxy
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func addStunnelProxies(contents []string, socksProxyHost string, socksProxyPort string) []string {
+	output := []string{}
+
+	socksRegex := regexp.MustCompile("execargs\\s*=.*")
+
+	for _, line := range contents {
+		if socksRegex.MatchString(line) {
+			output = append(output, fmt.Sprintf("%s -S %s:%s", line, socksProxyHost, socksProxyPort))
+		} else {
+			output = append(output, line)
+		}
+	}
+	return output
+}
+
+func removeStunnelProxies(contents []string) []string {
+	output := []string{}
+
+	socksRegex := regexp.MustCompile("(execargs\\s*=.*)-S [\\w.:-]+(.*)")
+
+	for _, line := range contents {
+		if socksRegex.MatchString(line) {
+			match := socksRegex.FindStringSubmatch(line)
+			output = append(output, strings.TrimSpace(fmt.Sprintf("%s %s", strings.TrimSpace(match[1]), strings.TrimSpace(match[2]))))
+		} else {
+			output = append(output, line)
+		}
+	}
+	return output
+}
--- proxy/stunnel_test.go
+++ proxy/stunnel_test.go
@@ -1,1 +1,162 @@
 package proxy
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestRemoveStunnelProxies(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []string
+		expected []string
+	}{
+		{
+			"removeStunnelProxies",
+			[]string{
+				"; stunnel pid",
+				"pid = /path/to/stunnel.pid ",
+				"",
+				"; Certs",
+				"cert = /path/to/client.p12",
+				"CAfile = /path/to/ca.pem",
+				"",
+				"; Protocol version (all, SSLv2, SSLv3, TLSv1)",
+				"sslVersion = TLSv1",
+				"verify = 2",
+				"",
+				"[group]",
+				"accept  = 127.0.0.1:1234",
+				"exec = /usr/local/bin/connect",
+				"execargs = /usr/local/bin/connect -w 5 -S socks.proxy-bastard.com:1085 internal.server 4321",
+				"client = yes",
+			},
+			[]string{
+				"; stunnel pid",
+				"pid = /path/to/stunnel.pid ",
+				"",
+				"; Certs",
+				"cert = /path/to/client.p12",
+				"CAfile = /path/to/ca.pem",
+				"",
+				"; Protocol version (all, SSLv2, SSLv3, TLSv1)",
+				"sslVersion = TLSv1",
+				"verify = 2",
+				"",
+				"[group]",
+				"accept  = 127.0.0.1:1234",
+				"exec = /usr/local/bin/connect",
+				"execargs = /usr/local/bin/connect -w 5 internal.server 4321",
+				"client = yes",
+			},
+		},
+	}
+	for _, c := range cases {
+		actual := removeStunnelProxies(c.input)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf(
+				`%s
+Call:
+removeStunnelProxies({{input}}) != {{expected}}
+
+Input:
+===============
+%s
+===============
+
+Expected:
+===============
+%s
+===============
+
+Actual:
+===============
+%s
+===============`,
+				c.name,
+				c.input,
+				c.expected,
+				actual)
+		}
+	}
+}
+
+func TestAddStunnelProxies(t *testing.T) {
+	socksProxyHost := "socks.proxy-bastard.com"
+	socksProxyPort := "1085"
+
+	cases := []struct {
+		name     string
+		input    []string
+		expected []string
+	}{
+		{
+			"removeStunnelProxies",
+			[]string{
+				"; stunnel pid",
+				"pid = /Users/stanta01/.stunnel/.stunnel.pid ",
+				"",
+				"; Certs",
+				"cert = /path/to/client.p12",
+				"CAfile = /path/to/ca.pem",
+				"",
+				"; Protocol version (all, SSLv2, SSLv3, TLSv1)",
+				"sslVersion = TLSv1",
+				"verify = 2",
+				"",
+				"[group]",
+				"accept  = 127.0.0.1:1234",
+				"exec = /usr/local/bin/connect",
+				"execargs = /usr/local/bin/connect -w 5 internal.server 4321",
+				"client = yes",
+			},
+			[]string{
+				"; stunnel pid",
+				"pid = /Users/stanta01/.stunnel/.stunnel.pid ",
+				"",
+				"; Certs",
+				"cert = /path/to/client.p12",
+				"CAfile = /path/to/ca.pem",
+				"",
+				"; Protocol version (all, SSLv2, SSLv3, TLSv1)",
+				"sslVersion = TLSv1",
+				"verify = 2",
+				"",
+				"[group]",
+				"accept  = 127.0.0.1:1234",
+				"exec = /usr/local/bin/connect",
+				fmt.Sprintf("execargs = /usr/local/bin/connect -w 5 internal.server 4321 -S %s:%s", socksProxyHost, socksProxyPort),
+				"client = yes",
+			},
+		},
+	}
+	for _, c := range cases {
+		actual := addStunnelProxies(c.input, socksProxyHost, socksProxyPort)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf(
+				`%s
+Call:
+addStunnelProxies({{input}}) != {{expected}}
+
+Input:
+===============
+%s
+===============
+
+Expected:
+===============
+%s
+===============
+
+Actual:
+===============
+%s
+===============`,
+				c.name,
+				c.input,
+				c.expected,
+				actual)
+		}
+	}
+}
