94b28820b0036055d7236fa46ef098fa8f796984
andystanton <andystanton@gmail.com>
2015-07-14 22:44:29 +0100

    Add test for file existence

--- proxy/scanner.go
+++ proxy/scanner.go
@@ -2,7 +2,10 @@ package proxy
 
 import (
 	"fmt"
+	"os"
 	"reflect"
+
+	"github.com/andystanton/proxybastard/util"
 )
 
 type lookupLocations struct {
@@ -21,13 +24,18 @@ var lookupThing = lookup{
 	},
 }
 
+// Scan scans for proxy targets.
 func Scan() {
 	reflected := reflect.ValueOf(lookupThing)
 	for i := 0; i < reflected.NumField(); i++ {
 		fmt.Println(reflected.Type().Field(i).Name)
 		lookupLocation, _ := reflected.Field(i).Interface().(lookupLocations)
+
 		for _, config := range lookupLocation.Configs {
-			fmt.Printf(" c %s\n", config)
+			fmt.Printf(" c %s\n", util.SanitisePath(config))
+			if _, err := os.Stat(util.SanitisePath(config)); err == nil {
+				fmt.Println("FOUND!")
+			}
 		}
 		for _, config := range lookupLocation.Executables {
 			fmt.Printf(" x %s\n", config)
