52d4aa42e7f999775d3ae38f625e0e83ef7b39f4
Andy Stanton <andystanton@gmail.com>
2015-07-16 13:47:47 +0100

    switch to reflection of definition rather than instance of configuration

--- proxy/scanner.go
+++ proxy/scanner.go
@@ -10,27 +10,6 @@ import (
 	"github.com/deckarep/golang-set"
 )
 
-var lookupConfiguration = Configuration{
-	Targets: &TargetsConfiguration{
-		NPM: &NPMConfiguration{
-			Enabled: true,
-			Files: []string{
-				"~/.npmrc",
-			},
-		},
-		APM: &APMConfiguration{
-			Enabled: true,
-			Files: []string{
-				"~/.atom/.apmrc",
-			},
-		},
-	},
-}
-
-func updateSuggestedConfiguration(suggestedConfiguration *Configuration, configurationToAdd Configuration) {
-
-}
-
 // Scan scans for proxy targets.
 func Scan() {
 	var suggestedConfiguration Configuration
@@ -41,54 +20,52 @@ func Scan() {
 	suggestedSOCKSProxyPorts := mapset.NewSet()
 	suggestedNonProxyHosts := mapset.NewSet()
 
-	if lookupConfiguration.Targets != nil {
+	targetsField := reflect.Indirect(reflect.ValueOf(&TargetsConfiguration{}))
+	for i := 0; i < targetsField.NumField(); i++ {
 
-		targetsField := reflect.Indirect(reflect.ValueOf(lookupConfiguration.Targets))
+		configurationField := reflect.New(reflect.TypeOf(targetsField.Field(i).Interface()).Elem()).Interface()
+		withConfig, hasConfig := configurationField.(WithConfig)
+		if hasConfig {
+			fieldName := targetsField.Type().Field(i).Name
 
-		for i := 0; i < targetsField.NumField(); i++ {
-
-			configurationField := reflect.New(reflect.TypeOf(targetsField.Field(i).Interface()).Elem()).Interface()
-			withConfig, hasConfig := configurationField.(WithConfig)
-			if hasConfig {
-				fieldName := targetsField.Type().Field(i).Name
-
-				if suggestedItemConfiguration := withConfig.suggestConfiguration(); suggestedItemConfiguration != nil {
-					if suggestedConfiguration.Targets == nil {
-						suggestedConfiguration.Targets = &TargetsConfiguration{}
-					}
-
-					if len(strings.TrimSpace(suggestedItemConfiguration.ProxyHost)) > 0 {
-						suggestedProxyHosts.Add(suggestedItemConfiguration.ProxyHost)
-					}
-
-					if len(strings.TrimSpace(suggestedItemConfiguration.ProxyPort)) > 0 {
-						suggestedProxyPorts.Add(suggestedItemConfiguration.ProxyPort)
-					}
-
-					if len(strings.TrimSpace(suggestedItemConfiguration.SOCKSProxyHost)) > 0 {
-						suggestedSOCKSProxyHosts.Add(suggestedItemConfiguration.SOCKSProxyHost)
-					}
-
-					if len(strings.TrimSpace(suggestedItemConfiguration.SOCKSProxyPort)) > 0 {
-						suggestedSOCKSProxyPorts.Add(suggestedItemConfiguration.SOCKSProxyPort)
-					}
-
-					for _, nph := range suggestedItemConfiguration.NonProxyHosts {
-						suggestedNonProxyHosts.Add(nph)
-					}
-
-					targetsField := reflect.Indirect(reflect.ValueOf(suggestedConfiguration.Targets))
-					targetsFieldSuggested := reflect.Indirect(reflect.ValueOf(suggestedItemConfiguration.Targets))
-
-					targetsField.FieldByName(fieldName).Set(targetsFieldSuggested.FieldByName(fieldName))
+			if suggestedItemConfiguration := withConfig.suggestConfiguration(); suggestedItemConfiguration != nil {
+				if suggestedConfiguration.Targets == nil {
+					suggestedConfiguration.Targets = &TargetsConfiguration{}
 				}
+
+				if len(strings.TrimSpace(suggestedItemConfiguration.ProxyHost)) > 0 {
+					suggestedProxyHosts.Add(suggestedItemConfiguration.ProxyHost)
+				}
+
+				if len(strings.TrimSpace(suggestedItemConfiguration.ProxyPort)) > 0 {
+					suggestedProxyPorts.Add(suggestedItemConfiguration.ProxyPort)
+				}
+
+				if len(strings.TrimSpace(suggestedItemConfiguration.SOCKSProxyHost)) > 0 {
+					suggestedSOCKSProxyHosts.Add(suggestedItemConfiguration.SOCKSProxyHost)
+				}
+
+				if len(strings.TrimSpace(suggestedItemConfiguration.SOCKSProxyPort)) > 0 {
+					suggestedSOCKSProxyPorts.Add(suggestedItemConfiguration.SOCKSProxyPort)
+				}
+
+				for _, nph := range suggestedItemConfiguration.NonProxyHosts {
+					suggestedNonProxyHosts.Add(nph)
+				}
+
+				targetsField := reflect.Indirect(reflect.ValueOf(suggestedConfiguration.Targets))
+				targetsFieldSuggested := reflect.Indirect(reflect.ValueOf(suggestedItemConfiguration.Targets))
+
+				targetsField.FieldByName(fieldName).Set(targetsFieldSuggested.FieldByName(fieldName))
 			}
 		}
 	}
+
 	marshalled, err := json.MarshalIndent(suggestedConfiguration, "", "    ")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Printf("Proxy host candidates: %s\n", suggestedProxyHosts)
 	fmt.Printf("Proxy port candidates: %s\n", suggestedProxyPorts)
 	fmt.Printf("SOCKS host candidates: %s\n", suggestedSOCKSProxyHosts)
