9975134c4314bdaef1371245b196f92e18d38f87
Andy Stanton <andystanton@gmail.com>
2015-07-16 10:16:39 +0100

    document next approach for scan

--- proxy/scanner.go
+++ proxy/scanner.go
@@ -56,6 +56,16 @@ func addFilesToSuggestConfiguration(suggestedConfiguration *Configuration, field
 func Scan() {
 	var suggestedConfiguration Configuration
 
+	// Alternate approach :
+	// - iterate over the fields of suggestedConfiguration.
+	// - create a new config item and cast it to WithConfig.
+	// - create some method on WithConfig interface that returns a *interface{}.
+	// - the method generates either nil or an item of the same type as config suggestion.
+	// - if the type is not nil, that is what's added to suggestedConfiguration.
+
+	// this would allow to have non-file based criteria for adding an item to suggestion
+	// which really does need to be the case for things like git or DM which just shell out.
+
 	if lookupConfiguration.Targets != nil {
 
 		targetsField := reflect.Indirect(reflect.ValueOf(lookupConfiguration.Targets))
